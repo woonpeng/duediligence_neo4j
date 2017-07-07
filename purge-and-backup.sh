bd9a995cd4616979b4d54c541ca553166d5e0629
Chris Sng <chrissng@users.noreply.github.com>
2017-07-07 18:49:31 +0800

    use lmgtfy's approach (#3)

--- purge-and-backup.sh
+++ purge-and-backup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -euo pipefail
 
 mkdir -p /data/backups && \
@@ -6,33 +6,38 @@ service neo4j stop && \
   pushd /data/backups && \
     max=4 && \
     /var/lib/neo4j/bin/neo4j-admin dump --database=graph.db --to=graph.db.dump.tmp && \
-    (for (( i=$max-2; i>=0; i-- )); do 
+    (for (( i=$max-2; i>=0; i-- )); do
     if [ -f graph.db.dump.$i ]; then mv graph.db.dump.$i graph.db.dump.$((i+1)); fi done && \
     if [ -f graph.db.dump ]; then mv graph.db.dump graph.db.dump.0; fi && \
     if [ -f graph.db.dump.tmp ]; then mv graph.db.dump.tmp graph.db.dump; fi) && popd && \
   pushd /data/databases && \
     rm -rf graph.db && popd && service neo4j start
 
-
-DB_TIMEOUT=180
-set +e
-# Wait for Neo4J to be available
-# Strategy from http://superuser.com/a/806331/98716
-echo "Checking database connection"
+# Wait for neo4j to start by trying to connect to the neo4j address and port
+# The official neo4j image starts neo4j once to check for correct credentials
+# stops it, and then starts it for real.
+NEO4J_TIMEOUT=60
 DATABASE_DEV="/dev/tcp/localhost/7687"
+echo "Checking database connection ${DATABASE_DEV}"
+timeout -t ${NEO4J_TIMEOUT} bash <<EOT
 
-timeout ${DB_TIMEOUT} bash <<EOT
-while ! (echo > "${DATABASE_DEV}") >/dev/null 2>&1; do
-    echo "Waiting for database ${DATABASE_DEV}"
-    sleep 2;
-done;
+neo4j_wait() {
+  while ! (echo > "${DATABASE_DEV}") >/dev/null 2>&1; do
+      echo "Waiting for database ${DATABASE_DEV}"
+      sleep 2;
+  done;
+
+  sleep 5
+}
+
+neo4j_wait
+neo4j_wait
 EOT
 RESULT=$?
 
 if [ ${RESULT} -eq 0 ]; then
-    echo "Neo4j database available"
+    echo "Neo4J database now available"
 else
-    echo "Neo4j database is not available"
+    echo "Neo4J database is not available"
     exit 1
 fi
-set -e
