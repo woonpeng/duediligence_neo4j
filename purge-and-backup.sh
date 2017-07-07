220dc387c4cd1ce5f5b29cb404d120e5328f19f1
chrissng <chris@data.gov.sg>
2017-07-07 18:13:39 +0800

    wait for db to start

--- purge-and-backup.sh
+++ purge-and-backup.sh
@@ -12,3 +12,27 @@ service neo4j stop && \
     if [ -f graph.db.dump.tmp ]; then mv graph.db.dump.tmp graph.db.dump; fi) && popd && \
   pushd /data/databases && \
     rm -rf graph.db && popd && service neo4j start
+
+
+DB_TIMEOUT=180
+set +e
+# Wait for Neo4J to be available
+# Strategy from http://superuser.com/a/806331/98716
+echo "Checking database connection"
+DATABASE_DEV="/dev/tcp/localhost/7687"
+
+timeout ${DB_TIMEOUT} bash <<EOT
+while ! (echo > "${DATABASE_DEV}") >/dev/null 2>&1; do
+    echo "Waiting for database ${DATABASE_DEV}"
+    sleep 2;
+done;
+EOT
+RESULT=$?
+
+if [ ${RESULT} -eq 0 ]; then
+    echo "Neo4j database available"
+else
+    echo "Neo4j database is not available"
+    exit 1
+fi
+set -e
