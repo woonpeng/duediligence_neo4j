2fd88b7d0a44b0a2eafefd003d00235d812f5175
fred4freedom <fredteo@data.gov.sg>
2017-07-10 14:01:43 +0800

    Restore service (#6)
    * Added restore endpoint to neo4j
    
    * Fix restore function bug: when the parameter "i" is not specified, restore will not work
    
    * remove extra line at the top

--- restore.sh
+++ restore.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 mkdir -p /data/backups && \
 service neo4j stop && \
   pushd /data/backups && \
-  if [ $# -eq 0 ] || [ $1 -eq 0 ] ; then \
+  if [ $# -eq 0 ] || [ -z "$1" ] || [ $1 -eq 0 ] ; then \
     if [ -f graph.db.dump ]; then /var/lib/neo4j/bin/neo4j-admin load --database=graph.db --from=graph.db.dump --force=true ; fi; \
   else \
     i=$1
