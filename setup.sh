b1fcfe7216e23a1281c815c37115161606ca6681
wp <deadstroke@gmail.com>
2020-03-19 13:55:46 +0800

    fix: this is how to properly append ssh keys!!!

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # adds modified key to neo4j_keys folder
 ./add_ssh_key.sh -n 1 -r ~/hmaster_keys/id_rsa.pub
-./add_ssh_key.sh -n 1 -r ~/hmaster-dd_keys/id_rsa.pub
+./add_ssh_key.sh -r ~/hmaster-dd_keys/id_rsa.pub
 
 docker run -itd \
 -p 7474:7474 -p 7687:7687 --network=my-bridge-network --name=neo4j finnet-neo4j
